c1dca678b52caea52d19f87b40c654a3fd6c897c
Sarthak patil <sarthakpatil4455@gmail.com>
2025-06-25 21:38:07 -0700

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,16 @@
+
+public class pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          int r=5;
+          for(int i=0;i<r;i++) {
+        	  for(int j=0;j<=i;j++) {
+        		  System.out.print("*");
+        	  }
+        	  System.out.println();
+          }
+	}
+
+}
+  
